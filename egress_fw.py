ff5398f50e6a6ccdf9316d043fb6760933c774c7
Nathan Reilly <nreilly@redhat.com>
2022-07-28 14:43:25 +1000

    Add default rules for apiservers

--- egress_fw.py
+++ egress_fw.py
@@ -60,7 +60,8 @@ sdn = json.loads(subprocess.run([ "oc", "get", "Network.config.openshift.io", "c
 
 apiservers = json.loads(subprocess.run([ "oc", "get", "ep", "kubernetes", "-n", "default", "-ojson" ], stdout=subprocess.PIPE).stdout)["subsets"]["addresses"]
 
-print (apiservers)
+print (sdn)
+print (json.dumps(apiservers, indent=2)
 
 #for apiserver in apiservers["addresses"]:
 #    for key, value in apiserver.items():
