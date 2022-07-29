77f6a67d40b2ac12291a5da480d2c72508deff0f
Nathan Reilly <nreilly@redhat.com>
2022-07-29 12:33:28 +1000

    Add default rules for apiservers

--- egress_fw.py
+++ egress_fw.py
@@ -57,7 +57,7 @@ DefaultAllowHosts = []
 
 domain_files = glob.glob(os.path.join(args.dir, args.glob))
 
-sdn = json.loads(subprocess.run([ "oc", "get", "Network.config.openshift.io", "cluster", "-ojson"], stdout=subprocess.PIPE).stdout) #["spec"]["networkType"]
+sdn = json.loads(subprocess.run([ "oc", "get", "Network.config.openshift.io", "cluster", "-ojson"], stdout=subprocess.PIPE).stdout)["spec"]["networkType"]
 
 apiservers = json.loads(subprocess.run([ "oc", "get", "ep", "kubernetes", "-n", "default", "-ojson" ], stdout=subprocess.PIPE).stdout)
 
